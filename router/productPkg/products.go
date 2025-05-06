5b6c5bfcb4c43054e0b3b375583d814764db3793
王元智 <208935434@qq.com>
2025-05-06 15:45:40 +0800

    one

--- router/productPkg/products.go
+++ router/productPkg/products.go
@@ -5,24 +5,25 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type ProductsRouter struct {}
+type ProductsRouter struct{}
 
 // InitProductsRouter 初始化 products表 路由信息
-func (s *ProductsRouter) InitProductsRouter(Router *gin.RouterGroup,PublicRouter *gin.RouterGroup) {
+func (s *ProductsRouter) InitProductsRouter(Router *gin.RouterGroup, PublicRouter *gin.RouterGroup) {
 	productsRouter := Router.Group("products").Use(middleware.OperationRecord())
 	productsRouterWithoutRecord := Router.Group("products")
 	productsRouterWithoutAuth := PublicRouter.Group("products")
 	{
-		productsRouter.POST("createProducts", productsApi.CreateProducts)   // 新建products表
-		productsRouter.DELETE("deleteProducts", productsApi.DeleteProducts) // 删除products表
+		productsRouter.POST("createProducts", productsApi.CreateProducts)             // 新建products表
+		productsRouter.DELETE("deleteProducts", productsApi.DeleteProducts)           // 删除products表
 		productsRouter.DELETE("deleteProductsByIds", productsApi.DeleteProductsByIds) // 批量删除products表
-		productsRouter.PUT("updateProducts", productsApi.UpdateProducts)    // 更新products表
+		productsRouter.PUT("updateProducts", productsApi.UpdateProducts)              // 更新products表
 	}
 	{
-		productsRouterWithoutRecord.GET("findProducts", productsApi.FindProducts)        // 根据ID获取products表
-		productsRouterWithoutRecord.GET("getProductsList", productsApi.GetProductsList)  // 获取products表列表
+		productsRouterWithoutRecord.GET("findProducts", productsApi.FindProducts)       // 根据ID获取products表
+		productsRouterWithoutRecord.GET("getProductsList", productsApi.GetProductsList) // 获取products表列表
 	}
 	{
-	    productsRouterWithoutAuth.GET("getProductsPublic", productsApi.GetProductsPublic)  // products表开放接口
+		productsRouterWithoutAuth.GET("getProductsPublic", productsApi.GetProductsPublic)             // products表开放接口
+		productsRouterWithoutAuth.GET("getStandardCategoryList", productsApi.GetStandardCategoryList) // products表开放接口
 	}
 }
